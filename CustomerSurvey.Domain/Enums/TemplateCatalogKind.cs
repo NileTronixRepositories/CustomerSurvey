1a9dbff738a25d45fd719715b22f609b51905927
LoaiSaberC4r <loai.sabir@outlook.com>
2026-06-03 19:34:02 +0300

    K

---
+++ CustomerSurvey.Domain/Enums/TemplateCatalogKind.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerSurvey.Domain.Enums
+{
+    public enum TemplateCatalogKind
+    {
+        Authorized = 1,
+        Anonymous = 2
+    }
+}
